f7fa9856c5d448f9cbfa3666f379ee06dbeac6e5
Christian Silva <ConcededGore@gmail.com>
2022-03-03 17:32:27 -0500

    ok NOW i think i fixed them

---
+++ auth-app/src/contexts/AuthContext.js
@@ -0,0 +1,33 @@
+import React, {useContext, useState, useEffect} from 'react';
+import { auth } from '../firebase';
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+		return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+	const [currentUser, setCurrentUser] = useState()
+
+	function signup(email, password) {
+		auth.createUserWithEmailAndPassord(email, password);
+	}
+
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged(user => {
+			setCurrentUser(user)
+		})
+		return unsubscribe;
+	}, [])
+
+	const value = {
+		currentUser
+	}
+
+	return (
+		<AuthContext.Provider>
+			{children}
+		</AuthContext.Provider>
+	)
+}

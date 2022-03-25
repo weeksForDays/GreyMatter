190c781f77759f047067c85f3955838dd5ef9274
Orion Schwab <orion.schwab@maine.edu>
2022-03-25 16:54:56 -0400

    Finally resolving the merge conflict

--- fly-away/src/components/Home.js
+++ fly-away/src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from "react-bootstrap";
 import '../css/login.css';
 import '../css/Home.css';
+import NewAccount from "./NewAccount";
 import {useUserAuth} from "../contexts/UserAuthContext";
 import {useNavigate} from 'react-router-dom';
 
@@ -27,7 +28,7 @@ const Home = () => {
 	return (
 		<div>
 			<div className="centeronscreen loginwidth">
-				<div className = "p-4 box mt-3 text-center">Hello World</div>
+				<div className = "p-4 box mt-3 text-center"><NewAccount/></div>
 				<div className = "d-grid gap-2">
 					<Button variant = "primary" onClick={handleLogOut}>Log Out</Button>
 					<Button variant = "secondary" onClick={handleFirestoreNav}>Firestore Demo</Button>
